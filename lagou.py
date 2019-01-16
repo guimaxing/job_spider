429a8b3c00fa472a772fee2afb4c532f9cddf72b
guimaxing <31811372+guimaxing@users.noreply.github.com>
2019-01-16 15:03:49 +0800

    first commit

---
+++ lagou.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 11:42:00 2019
+@author: Moc
+"""
+import pymysql
+import requests
+import time
+from lxml import etree
+from bs4 import BeautifulSoup
+#from urllib.parse import quote
+
+def parse_page(url,city_code):
+    try:
+        soup = requests_(url)
+        resultList = soup.find_all('div', id ='resultList')
+        con_result = etree.HTML(str(resultList).replace('\u3000','').replace('\n','').strip())
+        
+        job_list = [name.strip() for name in con_result.xpath('//div[@class="el"]/p[contains(@class,"t1")]/span/a//text()')]
+        job_url_list = con_result.xpath('//div[@class="el"]/p[contains(@class,"t1")]/span/a//@href')
+        company_list = con_result.xpath('//div[@class="el"]/span[contains(@class,"t2")]/a//text()')
+        company_url = con_result.xpath('//div[@class="el"]/span[contains(@class,"t2")]/a//@href')
+        location = con_result.xpath('//div[@class="el"]/span[contains(@class,"t3")]//text()')
+        salary = con_result.xpath('//div[@class="el"]/span[contains(@class,"t4")]//text()')
+        date = con_result.xpath('//div[@class="el"]/span[contains(@class,"t5")]//text()')
+
+        for i in range(len(job_list)):
+    #        data = [company,size,type_,company_url,eduLevel,emplType,jobName,jobTag,jobType,positionURL,rate,salary,workingExp]
+            insert_sql = """insert into	jobs_salary(job,job_url,company,company_url,location,salary,city_code,produce) 
+                                values ('{}','{}','{}','{}','{}','{}','{}','{}');
+                        """.format(job_list[i],job_url_list[i],company_list[i],company_url[i],location[i],salary[i],city_code,date[i])
+            print(company_list[i])
+            print(job_list[i])
+            print(salary[i])
+            print('\n')
+            try:
+                cur.execute(insert_sql)
+                conn.commit()
+            except Exception as e:
+                print(e)
+                pass
+    except Exception as e:
+        print(e)
+        pass
+    
+def requests_(url):
+    data = requests.get(url)
+    data.encoding = 'gb18030'
+    d = data.text
+    soup = BeautifulSoup(d, 'lxml')
+    return soup
+    
+def parse_main(url,pages,city_code,job):
+    for page in range(1,pages+1):
+        url_r = url.format(city_code=city_code,job=job,page=page)
+        print('开始爬取第{}页链接'.format(page+1))
+        print(url_r)
+        parse_page(url_r,city_code)
+        time.sleep(3)
+        
+if __name__ == '__main__':
+    conn = pymysql.connect(
+            host="127.0.0.1",
+            user="root",
+            password="mc940520",
+            db="lagou_jobs",
+            port=3306,
+            charset="utf8"
+        )
+    cur = conn.cursor()
+
+    url = "https://search.51job.com/list/{city_code},000000,0000,00,9,99,{job},2,{page}.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare="
+    pages=20  #共2000
+    job='%25E7%25A0%2594%25E5%258F%2591' #研发   '%25E6%258C%2596%25E6%258E%2598':挖掘
+    city_code_list = ['040000','010000','020000','030200']
+    #北京：010000; 全国：489,深圳：040000,上海：020000,广州：030200
+    city_code = '%252C'.join(city_code_list)
+    print(city_code)
+    parse_main(url,pages,city_code,job)
+    conn.close()
+
+
---
+++ zhilian.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 22:08:06 2019
+@author: Moc
+"""
+
+import pymysql
+import requests
+#import time
+#from urllib.parse import quote
+
+def parse_page(url,city_code):
+    try:
+        response = requests.get(url).json()
+        result = response['data']['results']
+        for r in result:
+            company = r['company']['name']
+            size = r['company']['size']['name']
+            type_ = r['company']['type']['name']
+            company_url = r['company']['url']
+            eduLevel = r['eduLevel']['name']
+            emplType = r['emplType']
+            jobName = r['jobName']
+            jobTag = r['jobTag']['searchTag']
+            jobType = r['jobType']['display']
+            positionURL = r['positionURL'] #招聘链接
+            rate = r['rate']  #反馈率
+            salary = r['salary'] #工资
+            workingExp = r['workingExp']['name'] #工作经验
+    #        data = [company,size,type_,company_url,eduLevel,emplType,jobName,jobTag,jobType,positionURL,rate,salary,workingExp]
+            insert_sql = """insert into	jobs_salary(company,size,type_,company_url,eduLevel,emplType,jobName,jobTag,jobType,positionURL,rate,salary,workingExp,city_code) 
+                                values ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}');
+                        """.format(company,size,type_,company_url,eduLevel,emplType,jobName,jobTag,jobType,positionURL,rate,salary,workingExp,city_code)
+            print(company)
+            print(positionURL)
+            print('\n')
+            try:
+                cur.execute(insert_sql)
+                conn.commit()
+            except Exception as e:
+                print(e)
+                pass
+    except Exception as e:
+        print(e)
+        pass
+    
+def parse_main(url,pages,city_code,job):
+    for page in range(pages):
+        p = page*90
+        url_r = url.format(page=p,city_code=city_code,job=job)
+        print('开始爬取第{}页链接'.format(page+1))
+        print(url_r)
+        parse_page(url_r,city_code)
+#        time.sleep(5)
+        
+if __name__ == '__main__':
+    conn = pymysql.connect(
+            host="127.0.0.1",
+            user="root",
+            password="mc940520",
+            db="zhilian_jobs",
+            port=3306,
+            charset="utf8"
+        )
+    cur = conn.cursor()
+    url ="https://fe-api.zhaopin.com/c/i/sou?start={page}&pageSize=90&cityId={city_code}&industry=10100&salary=0,0&workExperience=-1&education=-1&companyType=-1&employmentType=-1&jobWelfareTag=-1&kw={job}&kt=3&=0&_v=0.33977872&x-zp-page-request-id=b0434b03d11e4b9daf4cf3a887fbd121-1547573058264-851670"
+#    url = "https://fe-api.zhaopin.com/c/i/sou?start={page}&pageSize=90&cityId={city_code}&salary=0,0&workExperience=-1&education=-1&companyType=-1&employmentType=-1&jobWelfareTag=-1&kw={job}&kt=3&=0&_v=0.12367974&x-zp-page-request-id=4b1c9ae01d674decbc440cd60082872d-1547563110403-998176"
+    pages=12
+    job='%E7%A0%94%E5%8F%91' #研发
+    city_code_list = ['530','765','538','763']
+#    city_code='530' #北京：530; 全国：489,深圳：765,上海：538,广州：763
+    for city_code in city_code_list:
+        print(city_code)
+        parse_main(url,pages,city_code,job)
+    conn.close()
